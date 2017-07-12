5873e771bc586a57992655d933fefb171f728922
Office <laoniustudio@gmail.com>
2017-07-11 18:04:38 -0700

    no message

--- allied/settings.py
+++ allied/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'allied.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'root',
+        'PASSWORD': '1314530ab',
     }
 }
 
