ac377cb1c66c590dfb96388a5e3cdfde0335b067
sun <laoniustudio@gmail.com>
2017-07-11 23:23:15 -0700

    no message

--- allied/settings.py
+++ allied/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles') #for heroku
