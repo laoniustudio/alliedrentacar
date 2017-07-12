419375786f24cd3ce43129202d43387211efde3a
sun <laoniustudio@gmail.com>
2017-07-11 22:43:31 -0700

    no message

--- allied/settings.py
+++ allied/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import dj_database_url #for heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)) # for heroku
@@ -120,15 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-# Update database configuration with $DATABASE_URL. for heroku
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure() for heroku
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers for heroku
-ALLOWED_HOSTS = ['*']
 
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles') #for heroku
