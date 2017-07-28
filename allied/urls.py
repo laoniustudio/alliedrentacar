8323efcf37b774bd9133359b2d10ba97b4f31dee
Office <laoniustudio@gmail.com>
2017-07-28 11:57:00 -0700

    F: reset password send email page T: reset password done page

--- allied/urls.py
+++ allied/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     url(r'^api/',include('contentsAPI.urls')),
 
 
-    url(r'^password_reset/$', password_reset,{'template_name': 'accounting/password_reset.html'}, name='password_reset'),
+    url(r'^password_reset/$', password_reset, name='password_reset'),
     url(r'^password_reset/done/$', password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         password_reset_confirm, name='password_reset_confirm'),
