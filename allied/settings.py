6d8d4a08831633eeebd32af4d7372ac72f77009f
sun <laoniustudio@gmail.com>
2017-08-04 10:23:07 -0700

    no message

--- allied/settings.py
+++ allied/settings.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)) # for heroku
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -140,11 +140,11 @@ EMAIL_USE_TLS = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-        os.path.join(PROJECT_ROOT, "static"),
+        os.path.join(BASE_DIR, "static"),
     ]
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
